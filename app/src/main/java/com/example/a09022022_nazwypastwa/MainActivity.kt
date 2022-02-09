264fec72361e42ac99de9bc846c8daebf18060c2
Kakaoss <Kubek007.w@wp.pl>
2022-02-09 08:25:03 +0100

    Zadanie skończone

--- app/src/main/java/com/example/a09022022_nazwypastwa/MainActivity.kt
+++ app/src/main/java/com/example/a09022022_nazwypastwa/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.a09022022_nazwypastwa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.a09022022_nazwypastwa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,18 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        setupAutoCompleteTextView()
+    }
+
+    private fun setupAutoCompleteTextView() {
+        val countries = resources.getStringArray(R.array.countries_array)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
+        binding.autoCompleteTextView.setAdapter(adapter)
+
+        binding.btnSubmit.setOnClickListener {
+            val text = getString(R.string.entered_text) + " " + binding.autoCompleteTextView.text
+            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
